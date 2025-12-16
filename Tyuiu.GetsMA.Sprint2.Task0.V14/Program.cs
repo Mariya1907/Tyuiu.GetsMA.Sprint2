5e91f0331b8cd190bdfb8d08f9641e3dce2b7879
Mariya <Mariya@>
2025-12-16 20:48:54 +0500

    Добавьте файлы проекта.

--- Tyuiu.GetsMA.Sprint2.Task0.V14/Program.cs
+++ Tyuiu.GetsMA.Sprint2.Task0.V14/Program.cs
@@ -10,11 +10,12 @@ namespace Tyuiu.GetsMA.Sprint2.Task0.V14
             int x = 1075;
             int y = 754;
 
-            // Создаём объект через интерфейс
             ISprint2Task0V14 sprint = new ComparisonLogic();
 
+            // Получаем массив результатов, все вычисления происходят в библиотеке
             bool[] results = sprint.GetComparisonResults(x, y);
 
+            // Вывод в консоль (только отображение, без изменений)
             Console.WriteLine("Логическая последовательность:");
             foreach (var b in results)
             {
