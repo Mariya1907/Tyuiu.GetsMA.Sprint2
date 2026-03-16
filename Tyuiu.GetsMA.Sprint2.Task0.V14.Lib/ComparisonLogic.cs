2ecc6407492f8a592e98b3e769e6d58c4ba0cc5e
Mariya <Mariya@>
2026-03-16 18:42:42 +0500

    2

--- Tyuiu.GetsMA.Sprint2.Task0.V14.Lib/ComparisonLogic.cs
+++ Tyuiu.GetsMA.Sprint2.Task0.V14.Lib/ComparisonLogic.cs
@@ -1,21 +1,21 @@
-﻿namespace Tyuiu.GetsMA.Sprint2.Task0.V14.Lib
+﻿using System;
+using tyuiu.cources.programming.interfaces.Sprint2;  // ✅ Sprint2!
+
+namespace Tyuiu.GetsMA.Sprint2.Task0.V14.Lib
 {
-    // Реализация интерфейса ISprint2Task0V14
-    public class ComparisonLogic : ISprint2Task0V14
+    public class ComparisonLogic : ISprint2Task0V14  // ✅ Найдётся!
     {
-        public bool[] GetComparisonResults(int x, int y)
+        
+        public bool[] GetComparisonResults(int x, int y)  // ✅ Правильный метод!
         {
             bool[] results = new bool[6];
-
-            // Последовательность операций сравнения и арифметики
-            results[0] = x > y;             // true
-            results[1] = x < y;             // false
-            results[2] = (x - 321) >= y;    // true
-            results[3] = (x + 1000) < y;    // false
-            results[4] = x != y;            // true
-            results[5] = (y == 1000);       // false
-
-            return results;
+            results[0] = x > y;             // ✅ Логика правильная!
+            results[1] = x < y;
+            results[2] = (x - 321) >= y;
+            results[3] = (x + 1000) < y;
+            results[4] = x != y;
+            results[5] = (y == 1000);
+            return results;  // ✅ Массив 6 элементов!
         }
     }
 }
