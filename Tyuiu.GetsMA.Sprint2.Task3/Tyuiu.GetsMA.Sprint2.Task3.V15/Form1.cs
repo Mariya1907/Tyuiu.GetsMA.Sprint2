ba8015bdb76ccf3fc50d779e931840e83e04ef50
Mariya <Mariya@>
2026-03-17 23:07:55 +0500

    3

---
+++ Tyuiu.GetsMA.Sprint2.Task3/Tyuiu.GetsMA.Sprint2.Task3.V15/Form1.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.GetsMA.Sprint2.Task3.V15
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
