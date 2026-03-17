db08dc7bc520f5dfc7b916e022f1d711285451eb
Mariya <Mariya@>
2026-03-18 00:20:20 +0500

    1

--- Tyuiu.GetsMA.Sprint2.Task1.V1.Lib/ISprint2Task1V1.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Tyuiu.GetsMA.Sprint2.Task1.V1.Lib
-{
-    public interface ISprint2Task1V1
-    {
-        bool[] GetLogicResults(int a, int b, int c, int d);
-    }
-}
-
--- Tyuiu.GetsMA.Sprint2.Task1.V1/Program.cs
+++ Tyuiu.GetsMA.Sprint2.Task1.V1/Program.cs
@@ -1,26 +1,44 @@
 ﻿using System;
 using Tyuiu.GetsMA.Sprint2.Task1.V1.Lib;
 
+
 namespace Tyuiu.GetsMA.Sprint2.Task1.V1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int a = 119;
-            int b = 196;
-            int c = 134;
-            int d = 327;
+            DataService ds = new DataService();
+            
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Логические операции                                               *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу из операций сравнений (==, !=, <, >, <=, >=,         *");
+            Console.WriteLine("* последовательность можно чередовать, но использовать один раз в         *");
+            Console.WriteLine("* выражении) и логических операций (|, &, ||, &&, !, ^, последовательность*");
+            Console.WriteLine("* операций не должна нарушаться), а также арифметических выражений,       *");
+            Console.WriteLine("* которая вернет логическую последовательность(массив):(True, False, True,*");
+            Console.WriteLine("* False, True, False), при a = 119, b = 196, c = 134, d = 327             *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* a = 119, b = 196, c = 134, d = 327                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
 
-            ISprint2Task1V1 logic = new LogicOperations();
-
-            bool[] results = logic.GetLogicResults(a, b, c, d);
-
+            int a = 119, b = 196, c = 134, d = 327;
+            bool[] res = ds.GetLogicOperations(a, b, c, d);
+            for (int i = 0; i < res.Length; i++)
             {
+                Console.WriteLine(res[i]);
             }
-
-            Console.WriteLine("Нажмите любую клавишу для выхода...");
-            Console.ReadKey();
+            Console.ReadLine();
         }
     }
 }
